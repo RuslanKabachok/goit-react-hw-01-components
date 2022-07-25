d671a466f5f7c247a55e81bd8b8384f58ef6665c
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-07-25 19:37:41 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import user from 'user.json';
+import transactions from 'transaction.json';
+import friends from 'friends.json';
+import data from 'data.json';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { Friends } from './FriendsList/Friends';
+import { Transactions } from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        textTransform: 'uppercase',
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <Friends friends={friends} />
+      <Transactions items={transactions} />
     </div>
   );
 };
